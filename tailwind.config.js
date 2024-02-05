588f1b4ee860df74b221594e516f31cf3e8e0d2b
Diran sai <dirantechie@gmail.com>
2024-02-05 11:04:15 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        color1: "#091931",
+        color2: "#0a3051",
+        color3: "#21293c",
+        color4: "#4b587c"
+      }
+    },
+  },
+  plugins: [],
+}
+
